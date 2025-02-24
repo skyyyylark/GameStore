86daddbf4a2cf773da2a4d4e1d86b1bdafd9a525
skyyyylark <gmmagomedov04@gmail.com>
2025-02-24 19:28:03 +0600

    create service for user

---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using Abstractions.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<int> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user == null)
+                throw new UnauthorizedAccessException("Invalid username or password");
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (!result.Succeeded)
+                throw new UnauthorizedAccessException("Invalid username or password");
+            return StatusCodes.Status200OK;
+        }
+
+        public async Task<int> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return StatusCodes.Status200OK;
+        }
+
+        public async Task<int> Register(RegisterModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user == null)
+                throw new UnauthorizedAccessException("Invalid username or password");
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (!result.Succeeded)
+                throw new UnauthorizedAccessException("Invalid username or password");
+
+            return StatusCodes.Status200OK;
+        }
+    }
+}
