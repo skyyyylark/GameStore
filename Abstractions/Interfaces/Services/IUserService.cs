c99e36733c7738cebb41f64324823f983ddf2689
skyyyylark <gmmagomedov04@gmail.com>
2025-02-24 19:26:39 +0600

    create interface for user

---
+++ Abstractions/Interfaces/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractions.Interfaces.Services
+{
+    public interface IUserService
+    {
+        public Task<int> Register(RegisterModel model);
+        public Task<int> Login(LoginModel model);
+        public Task<int> Logout();
+    }
+}
