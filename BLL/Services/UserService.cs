510195ebfcd75eb6f4235e640891c601fceb8748
skyyyylark <gmmagomedov04@gmail.com>
2025-02-25 15:35:02 +0600

    add generation of access token

--- BLL/Services/UserService.cs
+++ BLL/Services/UserService.cs
@@ -1,10 +1,16 @@
 ﻿using Abstractions.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Models.Entities;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,24 +20,27 @@ namespace BLL.Services
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
-        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _configuration = configuration;
         }
 
-        public async Task<int> Login(LoginModel model)
+        public async Task<string> Login([FromBody] LoginModel model)
         {
             var user = await _userManager.FindByNameAsync(model.Username);
-            if (user == null)
+            if(user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            {
                 throw new UnauthorizedAccessException("Invalid username or password");
+            }
+            var token = GenerateAccessToken(user.UserName);
+            return new JwtSecurityTokenHandler().WriteToken(token);
 
-            var result = await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent: true, lockoutOnFailure: false);
-            if (!result.Succeeded)
-                throw new UnauthorizedAccessException("Invalid username or password");
-            return StatusCodes.Status200OK;
         }
+
 
         public async Task<int> Logout()
         {
@@ -51,5 +60,26 @@ namespace BLL.Services
 
             return StatusCodes.Status200OK;
         }
+        public JwtSecurityToken GenerateAccessToken(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userName),
+                new Claim(ClaimTypes.Role, "SuperAdmin") 
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(1),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecretKey"])),
+                    SecurityAlgorithms.HmacSha256)
+            );
+
+            return token;
+        }
+
     }
 }
