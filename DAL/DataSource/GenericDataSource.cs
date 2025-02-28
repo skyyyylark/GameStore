443cca5a8d7b65508f3409ad838e50a497460104
skyyyylark <gmmagomedov04@gmail.com>
2025-02-28 11:00:34 +0600

    add localization

--- DAL/DataSource/GenericDataSource.cs
+++ DAL/DataSource/GenericDataSource.cs
@@ -1,6 +1,7 @@
 ﻿using Abstractions.Interfaces.DataSources;
 using DAL.EntityFramework;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,11 @@ namespace DAL.DataSource
     public class GenericDataSource<T> : IGenericDataSource<T> where T : class
     {
         protected readonly AppDbContext _context;
-        public GenericDataSource(AppDbContext context)
+        private readonly IStringLocalizer _localizer;
+        public GenericDataSource(AppDbContext context, IStringLocalizerFactory factory)
         {
             _context = context;
+            _localizer = factory.Create("Common.Resources.SharedResource", "Common"); ;
         }
         protected DbSet<T> Set => _context.Set<T>();
 
@@ -35,7 +38,7 @@ namespace DAL.DataSource
         {
             T item = await Set.FindAsync(id);
             if (item == null)
-                throw new KeyNotFoundException("Сущность не найдена");
+                throw new KeyNotFoundException(_localizer["KeyNotFound"]);
             Set.Remove(item);
             await _context.SaveChangesAsync();
             return item;
@@ -45,7 +48,7 @@ namespace DAL.DataSource
         {
             T findingItem = await Set.FindAsync(item);
             if (findingItem == null)
-                throw new ArgumentNullException("Сущность null");
+                throw new ArgumentNullException(_localizer["ArgumentNull"]);
             await Set.AddAsync(findingItem);
             await _context.SaveChangesAsync();
             return item;
@@ -56,7 +59,7 @@ namespace DAL.DataSource
         {
             T item = await Set.FindAsync(id);
             if(item == null)
-                throw new KeyNotFoundException($"Сущность с id = {id} не найдена");
+                throw new KeyNotFoundException(_localizer["KeyNotFound"]);
             return item;    
         }
     }
